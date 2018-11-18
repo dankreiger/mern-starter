fb60992087495efa42161e627c916d1a9936a461
Dan Kreiger <dan@dankreiger.com>
2018-11-18 15:41:05 +0100

    feat: finish authentication and server side validation

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI: `mongodb://${process.env.MLAB_DB_USER}:${
     process.env.MLAB_DB_PASSWORD
-  }@ds057000.mlab.com:57000/puppyconnector`
+  }@ds057000.mlab.com:57000/puppyconnector`,
+  secretOrKey: `${process.env.SECRET_OR_KEY}`
 };
