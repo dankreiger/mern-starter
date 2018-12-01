b3421c76ff870815c1221aab204209f8a08ed7cd
Dan Kreiger <dan@dankreiger.com>
2018-12-01 15:53:42 +0100

    feat: add client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layouts/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <h1>test</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
